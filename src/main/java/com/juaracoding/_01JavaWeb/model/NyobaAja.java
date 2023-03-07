7dfe4b86a3f10100f7b4ebac22b492e651dec045
safrilefendilubis <safrilefendi20@gmail.com>
2023-03-07 19:20:22 +0700

    nyoba

---
+++ src/main/java/com/juaracoding/_01JavaWeb/model/NyobaAja.java
@@ -0,0 +1,12 @@
+package com.juaracoding._01JavaWeb.model;/*
+IntelliJ IDEA 2022.3.2 (Ultimate Edition)
+Build #IU-223.8617.56, built on January 26, 2023
+@Author User a.k.a. Safril Efendi Lubis
+Java Developer
+Created on 3/7/2023 7:19 PM
+@Last Modified 3/7/2023 7:19 PM
+Version 1.1
+*/
+
+public class NyobaAja {
+}
